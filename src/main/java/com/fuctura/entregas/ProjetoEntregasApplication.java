0e01b02b0b4e5f56e6c38b7c46b5bce7e34ff073
caiocipriano <caio.silvax@hotmail.com>
2022-03-14 11:04:23 -0300

    frist commit 04/11

---
+++ src/main/java/com/fuctura/entregas/ProjetoEntregasApplication.java
@@ -0,0 +1,13 @@
+package com.fuctura.entregas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoEntregasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoEntregasApplication.class, args);
+	}
+
+}
