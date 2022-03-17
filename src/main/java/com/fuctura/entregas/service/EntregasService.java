1217bb1069cd8124568f42c63e3ddc155e9bc7c5
caiocipriano <caio.silvax@hotmail.com>
2022-03-17 09:44:54 -0300

    assigning HTTP methods in Service

--- src/main/java/com/fuctura/entregas/service/EntregasService.java
+++ src/main/java/com/fuctura/entregas/service/EntregasService.java
@@ -5,34 +5,70 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.fuctura.entregas.models.Cliente;
 import com.fuctura.entregas.models.Entrega;
+import com.fuctura.entregas.repository.ClienteRepository;
 import com.fuctura.entregas.repository.EntregasRepository;
 
 @Service
 public class EntregasService {
 	
 	@Autowired
-	EntregasRepository repository;
+	EntregasRepository entregasRepository;
 	
-	public List<Cliente> listClient(){
-		return repository.findAll();
+	@Autowired
+	ClienteRepository clienteRepository;
+	
+	//Cliente HTTP methods
+	
+	public List<Cliente> listClients(){
+		return clienteRepository.findAll();
+	}
+	
+	public Cliente findClient(@PathVariable Long id){
+		return this.clienteRepository.findById(id).get();
+	}
+	
+	public Cliente postCliente(@RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
+	
+	public Cliente putCliente(@RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
+	
+	public void deleteCliente(Long id){
+		clienteRepository.deleteById(id);
 	}
 	
 	
+	//Entrega HTTP methods
 	
-	//Função a ser chamada no Controller 
+	public List<Entrega> listEntregas(){
+		return entregasRepository.findAll();
+	}
+	
+	public Entrega findEntrega(@PathVariable Long id) {
+		return entregasRepository.findById(id).get();
+	}
+	
 	public Entrega postEntrega(@RequestBody Entrega entrega) {
 		entrega.setDataPedido(LocalDateTime.now());
 		entrega.setDataFinalizacao(LocalDateTime.now());
-		return repository.save(entrega);
+		return entregasRepository.save(entrega);
 	}
 	
+	
+	//Update Delivery Status and End Date and Time
 	public Entrega putEntrega(@RequestBody Entrega entrega) {
-		return repository.save(entrega);
+		return entregasRepository.save(entrega);
+	}
+	
+	public void deleteEntrega(Long id) {
+		entregasRepository.deleteById(id);
 	}
 	
 	
