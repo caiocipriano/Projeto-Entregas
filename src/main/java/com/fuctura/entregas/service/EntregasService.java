13373abc61e412f978f027ea10381a738394fe0a
caiocipriano <caio.silvax@hotmail.com>
2022-03-17 09:27:51 -0300

    assigning HTTP methods in Service

--- src/main/java/com/fuctura/entregas/service/EntregasService.java
+++ src/main/java/com/fuctura/entregas/service/EntregasService.java
@@ -1,12 +1,14 @@
 package com.fuctura.entregas.service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.fuctura.entregas.models.Cliente;
 import com.fuctura.entregas.models.Entrega;
 import com.fuctura.entregas.repository.EntregasRepository;
 
@@ -15,6 +17,12 @@ public class EntregasService {
 	
 	@Autowired
 	EntregasRepository repository;
+	
+	public List<Cliente> listClient(){
+		return repository.findAll();
+	}
+	
+	
 	
 	//Função a ser chamada no Controller 
 	public Entrega postEntrega(@RequestBody Entrega entrega) {
